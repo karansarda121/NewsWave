30d66ed43eea115d029333bc1662cce589fc15da
karansarda121 <166016376+karansarda121@users.noreply.github.com>
2024-06-01 18:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function reload() {
 
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-    const data = await res.json();
+    //const data = await res.json();
     bindData(data.articles);
 }
 
